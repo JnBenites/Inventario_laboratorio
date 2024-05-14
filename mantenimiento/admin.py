2b77586a4d27b875713f4f9873659da3e9fcfdef
Juan B <55644440+JnBenites@users.noreply.github.com>
2024-05-13 22:37:05 -0500

    👍

--- mantenimiento/admin.py
+++ mantenimiento/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Encargado, Laboratorio, Estado, TipoMantenimiento, Equipo, Mantenimiento
-
+from django.contrib.auth.admin import UserAdmin
+from .models import Laboratorio, Estado, TipoMantenimiento, Equipo, Mantenimiento
+from .models import Case, Monitor,Teclado, Raton,Parlante, Impresora, Proyector, Ups
 # Register your models here.
 
-admin.site.register(Encargado)
 admin.site.register(Laboratorio)
 admin.site.register(Estado)
 admin.site.register(TipoMantenimiento)
 admin.site.register(Equipo)
 admin.site.register(Mantenimiento)
+
+# computador
+admin.site.register(Case)
+admin.site.register(Monitor)
+admin.site.register(Teclado)
+admin.site.register(Raton)
+admin.site.register(Parlante)
+admin.site.register(Impresora)
+admin.site.register(Proyector)
+admin.site.register(Ups)
+
+
+admin.site.site_header = 'Inventario 😆'
+admin.site.site_title = 'Inventario 😂'
--- mantenimiento/models.py
+++ mantenimiento/models.py
@@ -1,54 +1,136 @@
-from django.db import models
-
-# Create your models here.
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Encargado(models.Model):
-    IdEncargados = models.AutoField(primary_key=True)
+    IdEncargado = models.AutoField(primary_key=True)
     Cedula = models.CharField(max_length=10)
-    Nombres = models.CharField(max_length=100)
-    Apellidos = models.CharField(max_length=100)
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f"{self.Nombres} {self.Apellidos}"
+        return f'{self.user.last_name} {self.user.first_name} - {self.user.email}'
 
 class Laboratorio(models.Model):
     IdLaboratorio = models.AutoField(primary_key=True)
-    Numero = models.CharField(max_length=100)
+    Nombre = models.CharField(max_length=100)
     Codigo = models.CharField(max_length=100)
-    Responsable = models.ForeignKey(Encargado, on_delete=models.PROTECT)
+    Responsable = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.Codigo
+        return f'{self.Codigo} {self.Nombre} - {self.Responsable.first_name} {self.Responsable.last_name}'
 
 class Estado(models.Model):
     IdEstado = models.AutoField(primary_key=True)
     Descripcion = models.CharField(max_length=100)
+
     def __str__(self):
         return self.Descripcion
 
 class TipoMantenimiento(models.Model):
     IdTipoMantenimiento = models.AutoField(primary_key=True)
     Descripcion = models.CharField(max_length=100)
+
     def __str__(self):
         return self.Descripcion
 
+class Case(models.Model):
+    IdCase = models.AutoField(primary_key=True)
+    CaseSerial = models.CharField(max_length=200)
+    Observacion = models.TextField()
+    FechaIngreso = models.DateField()
+
+    def __str__(self):
+        return str(self.CaseSerial)
+
+class Monitor(models.Model):
+    IdMonitor = models.AutoField(primary_key=True)
+    MonitorSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Resolucion = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+
+    def __str__(self):
+        return str(self.MonitorSerial)
+
+class Teclado(models.Model):
+    IdTeclado = models.AutoField(primary_key=True)
+    TecladoSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Distribucion = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+
+    def __str__(self):
+        return str(self.TecladoSerial)
+
+class Raton(models.Model):
+    IdRaton = models.AutoField(primary_key=True)
+    RatonSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+    def __str__(self):
+        return str(self.RatonSerial)
+
+class Parlante(models.Model):
+    IdParlante = models.AutoField(primary_key=True)
+    ParlanteSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+    def __str__(self):
+        return str(self.ParlanteSerial)
+
+class Impresora(models.Model):
+    IdImpresora = models.AutoField(primary_key=True)
+    ImpresoraSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+    def __str__(self):
+        return str(self.IdImpresora)
+
+class Proyector(models.Model):
+    IdProyector = models.AutoField(primary_key=True)
+    ProyectorSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+    def __str__(self):
+        return str(self.IdProyector)
+
+class Ups(models.Model):
+    IdUps = models.AutoField(primary_key=True)
+    UpsSerial = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=50)
+    Observacion = models.CharField(max_length=200)
+    FechaIngreso = models.DateField()
+    def __str__(self):
+        return str(self.IdUps)
+
 class Equipo(models.Model):
     IdEquipo = models.AutoField(primary_key=True)
-    Marca = models.CharField(max_length=100)
-    Modelo = models.CharField(max_length=100)
     SistemaOperativo = models.CharField(max_length=100)
     Descripcion = models.CharField(max_length=500)
     Estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+    Case = models.ForeignKey(Case, on_delete=models.PROTECT, null=True)
+    Monitor = models.ForeignKey(Monitor, on_delete=models.PROTECT, null=True)
+    Teclado = models.ForeignKey(Teclado, on_delete=models.PROTECT, null=True)
+    Raton = models.ForeignKey(Raton, on_delete=models.PROTECT, null=True)
+    Parlante = models.ForeignKey(Parlante, on_delete=models.PROTECT, null=True)
+    
     def __str__(self):
-        return f"{self.IdEquipo} - {self.Marca}"
-
+        return f'{self.IdEquipo} - {self.SistemaOperativo} - {self.Descripcion}'
 
 class Mantenimiento(models.Model):
     IdMantenimiento = models.AutoField(primary_key=True)
     IdTipoMantenimiento = models.ForeignKey(TipoMantenimiento, on_delete=models.PROTECT)
-    IdEquipo = models.ForeignKey(Equipo, on_delete=models.PROTECT)
+    IdEquipo = models.ForeignKey(Equipo, on_delete=models.PROTECT,  null=True)
     FechaIngreso = models.DateField()
     FechaSalida = models.DateField()
     DescripcionMantenimiento = models.CharField(max_length=500)
+
     def __str__(self):
-        return f"{self.IdTipoMantenimiento} - {self.IdEquipo} -  {self.FechaIngreso} - {self.FechaSalida} - {self.DescripcionMantenimiento}"
+        return f'{self.IdTipoMantenimiento.Descripcion} - {self.IdEquipo} - {self.FechaIngreso} - {self.FechaSalida} - {self.DescripcionMantenimiento}'
+
+
